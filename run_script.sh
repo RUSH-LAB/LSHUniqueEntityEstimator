4c3d223bb996de7cd5e2af1382bd475f2d422b00
Beidi Chen <beidichen@Beidis-MacBook-Pro.local>
2018-04-09 22:09:11 -0500

    plot

--- run_script.sh
+++ run_script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# g++-7 C++Codes/*.cpp -o output  -fopenmp
+g++-7 C++Codes/*.cpp -o output  -fopenmp
 
+
+# For Restaurant
 # for ((i=1;i<=10;i++)) ; 
 # 	do ./output config.txt; python pipeline.py --id 1 --input Restaurant_pair.csv --goldstan data/Restaurant.csv --output test ; 
 # done
@@ -8,8 +10,9 @@
 # python plot.py --input test --gt 753
 
 
-for ((i=1;i<=1;i++)) ; 
+# For CD
+for ((i=1;i<=10;i++)) ; 
 	do ./output config.txt; python pipeline.py --id 1 --delimiter ';' --trainsize 0.1 --input Restaurant_pair.csv --goldstan data/Cd.csv --output test ; 
 done
 
-# python plot.py --input test --gt 753
+python plot.py --input test --gt 9508
