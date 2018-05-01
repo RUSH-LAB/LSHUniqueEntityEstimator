be0f3ed518e94d1c54435165c558b5b56ad66a2b
Rebecca Steorts <beka@stat.duke.edu>
2018-05-01 15:42:39 -0400

    updated files to run with BC.

--- run_script.sh
+++ run_script.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-g++ -std=c++11 C++Codes/*.cpp -o output  -fopenmp
+#g++ -std=c++11 C++Codes/*.cpp -o output -fopenmp
+g++-7 -std=c++11 C++Codes/*.cpp -o output -fopenmp
 
 #For Restaurant
  for ((i=4;i<=16;i+=4)) ; 
 	do for ((j=1;j<=10; j++));
- 		do ./output config_restaurant.txt 1 $i; python pipeline.py --flag 0 --id $i --trainsize 0.5 --input data/Restaurant_pair.csv --goldstan data/Restaurant.csv --output test ; 
+ 		do ./output config_restaurant.txt 1 $i; python pipeline.py --flag 0 --id $i --trainsize 0.5 --input Restaurant_pair.csv --goldstan data/Restaurant.csv --output test ; 
  	done
  done
 
