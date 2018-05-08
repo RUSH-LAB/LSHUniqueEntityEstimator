3daf00eed2dff4b83fe19bf9fa9b4c91001446c8
keroro824 <bettychen824@gmail.com>
2018-05-08 08:53:20 -0700

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
-#g++ -std=c++11 C++Codes/*.cpp -o output -fopenmp
-g++-7 -std=c++11 C++Codes/*.cpp -o output -fopenmp
+g++ -std=c++11 C++Codes/*.cpp -o output -fopenmp
 
 #For Restaurant
- for ((i=4;i<=16;i+=4)) ; 
+for ((i=4;i<=16;i+=4)) ; 
 	do for ((j=1;j<=10; j++));
  		do ./output config_restaurant.txt 1 $i; python pipeline.py --flag 0 --id $i --trainsize 0.5 --input Restaurant_pair.csv --goldstan data/Restaurant.csv --output test ; 
- 	done
- done
+	done
+done
 
- python plot.py --input test --gt 753
+python plot.py --input test --gt 753
 
-
-# For CD
-#for ((i=1;i<=10;i++)) ; 
-#	do ./output config.txt; python pipeline.py --id 1 --delimiter ';' --trainsize 0.1 --input data/Restaurant_pair.csv --goldstan data/Cd.csv --output test ; 
-#done
-
-#python plot.py --input test --gt 9508
