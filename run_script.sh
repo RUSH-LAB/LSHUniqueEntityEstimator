0227c2623b868fd161b596a559282c232fa4ba29
Chen <beidchen@186590d46885.ant.amazon.com>
2018-05-20 11:15:26 -0700

    run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-# g++-5 -std=c++11 C++Codes/*.cpp -o output -fopenmp
+g++-5 -std=c++11 C++Codes/*.cpp -o output -fopenmp
 
-# For Restaurant
-# for ((i=6;i<=25;i+=6)) ; 
-# 	do for ((j=1;j<=10; j++));
-#  		do ./output config_restaurant.txt 1 $i; python pipeline.py --flag 0 --id $i --trainsize 0.3 --input Restaurant_pair.csv --goldstan data/Restaurant.csv --output test ; 
-# 	done
-# done
-
-# python plot.py --input test --gt 753
-
-#For CD
-for ((i=6;i<=20;i+=4)) ; 
-	do for ((j=1;j<=3; j++));
- 		do ./output config_cd.txt 1 $i; python pipeline.py --flag 0 --id $i --trainsize 0.5 --input Cd_pair.csv --goldstan replicate/cd.csv --delimiter ';' --output test ; 
+For Restaurant
+for ((i=6;i<=25;i+=6)) ; 
+	do for ((j=1;j<=10; j++));
+ 		do ./output config_restaurant.txt 1 $i; python pipeline.py --flag 0 --id $i --trainsize 0.3 --input Restaurant_pair.csv --goldstan data/Restaurant.csv --output test ; 
 	done
 done
 
-python plot.py --input test --gt 9508
+python plot.py --input test --gt 753
+
+#For CD
+# for ((i=6;i<=20;i+=4)) ; 
+# 	do for ((j=1;j<=3; j++));
+#  		do ./output config_cd.txt 1 $i; python pipeline.py --flag 0 --id $i --trainsize 0.5 --input Cd_pair.csv --goldstan replicate/cd.csv --delimiter ';' --output test ; 
+# 	done
+# done
+
+# python plot.py --input test --gt 9508
 
 #For Voter
 # for ((i=25;i<=40;i+=5)) ; 
