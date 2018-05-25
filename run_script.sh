86e2545cfe4d34340e1dfa7443a173fd38647f0c
Rebecca Steorts <beka@stat.duke.edu>
2018-05-25 09:44:46 -0400

    Update run_script.sh
    Adding comments

--- run_script.sh
+++ run_script.sh
@@ -1,15 +1,18 @@
+# General script to run the public and private data sets
+# Written by BC and RCS
+
 #!/bin/bash
 
-g++-5 -std=c++11 C++Codes/*.cpp -o output -fopenmp
+g++-7 -std=c++11 C++Codes/*.cpp -o output -fopenmp
 
-# For Restaurant
-# for ((i=6;i<=25;i+=6)) ; 
-# 	do for ((j=1;j<=10; j++));
-#  		do ./output config_restaurant.txt 1 $i; python pipeline.py --flag 0 --id #$i --trainsize 0.3 --input restaurant_pair.csv --goldstan data/restaurant.csv --output #log-restaurant ; 
-# 	done
-# done
+ For Restaurant
+ for ((i=6;i<=25;i+=6)) ; 
+ 	do for ((j=1;j<=10; j++));
+  		do ./output config_restaurant.txt 1 $i; python pipeline.py --flag 0 --id #$i --trainsize 0.3 --input restaurant_pair.csv --goldstan data/restaurant.csv --output #log-restaurant ; 
+ 	done
+ done
 
-# python plot.py --input log-restaurant --gt 753
+ python plot.py --input log-restaurant --gt 753
 
 #For CD
 # for ((i=6;i<=20;i+=4)) ; 
@@ -32,8 +35,8 @@ g++-5 -std=c++11 C++Codes/*.cpp -o output -fopenmp
 #For Syria
 # python preprocess.py
 
-for ((i=1;i<=10;i++)) ; 
-	do ./output config_syria.txt 15 10; python pipeline_for_syria.py --input syria_pair.csv --output log-syria --rawdata data/syria.csv --goldstandpair data/syria_train.csv; 
-done
+#for ((i=1;i<=10;i++)) ; 
+#	do ./output config_syria.txt 15 10; python pipeline_for_syria.py --input syria_pair.csv --output log-syria --rawdata data/syria.csv --goldstandpair data/syria_train.csv; 
+#done
 
-python count.py --input log-syria
+#python count.py --input log-syria
